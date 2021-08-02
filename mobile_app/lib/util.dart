4b97d77ea820ca0340fdcddbda6046c9668c26f7
robin <robinseo.seo@gmail.com>
2021-08-02 17:45:20 +0900

    Changed return types

--- mobile_app/lib/util.dart
+++ mobile_app/lib/util.dart
@@ -2,38 +2,34 @@ import 'dart:convert';
 import 'dart:io';
 import 'dart:typed_data';
 
-
 const int bufferSize = 4096;
-
 
 // readSize reads first 4 bytes from the reader and convert them into a uint32 value
 //
-// return [uint32, error (bool)]
-List readSize(RawSocket reader) {
-  // Read first 4 bytes for the size
-  List size = readNBytes(reader, 4);
-  var b = size.first;
-  var err = size.last;
-  print('size of file ' + b + 'error msg: ' + err);
+// return packet size (uint32)
+int readSize(RawSocket reader) {
+  // Read first 4 bytes for the packet size
+  Uint8List packetSize = readNBytes(reader, 4);
+  print('size of file ' + byteToUint32(packetSize).toString());
 
-  // convert them into a unit32value
-  if (err == true) {
-    return [byteToUint32(b), true];
+  if (packetSize == null) {
+    print("Error while reading packet size");
+    return 0;
   }
-  print("Error while reading packet size");
-  return [0, false];
+  // convert them into a unit32value
+  return byteToUint32(packetSize);
 }
 
 // readNBytes reads up to nth byte
 //
-// returns <[] bytes, error>
-List readNBytes(RawSocket reader, int n) {
-  Uint8List data = reader.read(n); // store in file
+// return data(8-bit unsigned integers)
+Uint8List readNBytes(RawSocket reader, int n) {
+  Uint8List data = reader.read(n); // read up to n byte
   if (data == null) {
     print("Error while reading bytes");
-    return [-1, false];
+    return null;
   }
-  return [data, true];
+  return data;
 }
 
 // Byte to unsigned int32
@@ -47,12 +43,10 @@ int byteToUint32(Uint8List value) {
 Uint8List uint32ToByte(int value) =>
     Uint8List(4)..buffer.asByteData().setInt32(0, value, Endian.big);
 
-
-
 // WriteString writes message to writer
 // length of message cannot exceed BufferSize
 // returns <total bytes sent, error>
-List writeString (RawSocket writer, String msg) {
+List writeString(RawSocket writer, String msg) {
   try {
     // Convert string to byte
     // * actual information converted into byte
@@ -60,9 +54,9 @@ List writeString (RawSocket writer, String msg) {
     // Get size(uint32) of total bytes to send
     var size = uint32ToByte(bytes.length);
     writer.write(size + bytes);
-    return [size ,true];
+    return [size, true];
   } catch (error) {
-    return [0 ,false];
+    return [0, false];
   }
 }
 
@@ -91,52 +85,57 @@ Future<List> writeBinary(RawSocket conn, File file) async {
     conn.write(await file.readAsBytes());
   } catch (error) {
     print("Unknown error in send_bin" + error);
-    return [0,false];
+    return [0, false];
   }
-  return [sizeInByte,true];
+  return [sizeInByte, true];
 }
-
-
-
-
 
 // readString reads string from a connection
 //
-// returns <String, int>
+// returns <String, status>
 List readString(RawSocket reader) {
-  var size = readSize(reader).first;
-  var err = readSize(reader).last;
-  if (err == false) {
-    print('Error while reading string size');
-  }
+  String string = "";
+  try {
+    int size = readSize(reader);
+    if (size == 0) {
+      print('Error while reading size');
+      return [string, false];
+    }
+    // ReadString always expect the size to be <= bufferSize
+    if (size > bufferSize) {
+      print("String size cannot be greater than " +
+          bufferSize.toString() +
+          ". String size: " +
+          size.toString());
+      return ["", false];
+    } else {
+      // Read string from the packet
+      Uint8List data = reader.read(size);
+      string = utf8.decode(data);
+      print(string);
 
-  // ReadString always expect the size to be <= bufferSize
-  if (size > bufferSize) {
-    print("String size cannot be greater than " + bufferSize.toString() + ". String size: " + size);
-    return ["", false];
+      // // if the above else statement doesn't work. Try to uncomment the line below and try it
+      // string = utf8.decode(readNString(reader, size).first);
+      // var err = readNBytes(reader, size).last;
+      // if (err == false) {
+      //   print("Error while reading string");
+      //   return ["", false];
+      // }
+    }
+  } catch (error) {
+    print("Unknown error in readString");
+    return [string, false];
   }
-
-  // Read string from the packet
-  String str = readNString(reader, size).first;
-  err = readNBytes(reader, size).last;
-  if (err == false) {
-    print("Error while reading string");
-    return ["", false];
-  }
-  print(str);
-  return [str, true];
+  return [string, true];
 }
 
 // readNString reads up to nth character. Maximum length should not exceed bufferSize.
-// return string, error
+// return [Uint8List, status]
 List readNString(RawSocket reader, int n) {
   if (n > bufferSize) {
     print("n should be smaller than " + bufferSize.toString());
     return ["", false];
   }
-  var buffer = readNBytes(reader, n).first;
-  var err = readNBytes(reader, n).last;
-  return [buffer.toString(), err];
+  Uint8List buffer = readNBytes(reader, n);
+  return [buffer, true];
 }
-
-
