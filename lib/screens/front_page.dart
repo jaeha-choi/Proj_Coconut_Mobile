1e20603bed9fbaaa988840d32888af89e4168995
Jaeha Choi <jaeha@mail.jaeha.dev>
2021-10-15 02:19:54 -0700

    Fix client null issue (wip)
    Client null issue was fixed, but the duplicate client init call issue remains.

--- lib/screens/front_page.dart
+++ lib/screens/front_page.dart
@@ -18,20 +18,13 @@ class FrontPage extends StatefulWidget {
 
 class _FrontPageState extends State<FrontPage> {
   late Client client;
+  bool init = false;
 
-  // _FrontPageState(this.client);
-
-  @override
-  void initState() {
-    createClient().then((result) {
-      setState(() {
-        client = result;
-      });
-    });
-  }
-
-  _getThingsOnStartup() async {
-    return await createClient();
+  Future initClient() async {
+    if (!this.init) {
+      this.client = await createClient();
+      this.init = true;
+    }
   }
 
   void _onItemTapped(int index) {
@@ -45,61 +38,52 @@ class _FrontPageState extends State<FrontPage> {
 
   @override
   Widget build(BuildContext context) {
-    // Widget widget = Container();
-
-    // switch (_index) {
-    //   // Contacts
-    //   case 0:
-    //     widget = Contacts(client: client);
-    //     break;
-    //   // Album
-    //   case 1:
-    //     widget = Album(client: client);
-    //     break;
-    //   case 2:
-    //   // widget = ();
-    // }
-
-    if (client == null) {
-      // This is what we show while we're loading
-      return new Container();
-    }
-
-    return SafeArea(
-      child: Scaffold(
-          body: IndexedStack(
-            index: _index,
-            children: <Widget>[
-              Contacts(client: client),
-              Album(client: client),
-              SelectFile(client: client),
-            ],
-          ),
-          bottomNavigationBar: BottomNavigationBar(
-            iconSize: 30,
-            selectedFontSize: 15,
-            selectedIconTheme: IconThemeData(color: Colors.teal, size: 30),
-            selectedLabelStyle:
-                TextStyle(fontWeight: FontWeight.bold, color: Colors.teal),
-            backgroundColor: Colors.black54,
-            elevation: 0,
-            items: const <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                icon: const Icon(Icons.account_circle),
-                label: 'Contacts',
-              ),
-              BottomNavigationBarItem(
-                icon: const Icon(Icons.photo_album),
-                label: 'Album',
-              ),
-              BottomNavigationBarItem(
-                icon: const Icon(Icons.folder),
-                label: 'File',
-              )
-            ],
-            currentIndex: _index,
-            onTap: _onItemTapped,
-          )),
+    return Scaffold(
+      body: FutureBuilder(
+          future: initClient(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return Scaffold(
+                  bottomNavigationBar: BottomNavigationBar(
+                    iconSize: 30,
+                    selectedFontSize: 15,
+                    selectedIconTheme:
+                        IconThemeData(color: Colors.teal, size: 30),
+                    selectedLabelStyle: TextStyle(
+                        fontWeight: FontWeight.bold, color: Colors.teal),
+                    backgroundColor: Colors.black54,
+                    elevation: 0,
+                    items: const <BottomNavigationBarItem>[
+                      BottomNavigationBarItem(
+                        icon: const Icon(Icons.account_circle),
+                        label: 'Contacts',
+                      ),
+                      BottomNavigationBarItem(
+                        icon: const Icon(Icons.photo_album),
+                        label: 'Album',
+                      ),
+                      BottomNavigationBarItem(
+                        icon: const Icon(Icons.folder),
+                        label: 'File',
+                      )
+                    ],
+                    currentIndex: _index,
+                    onTap: _onItemTapped,
+                  ),
+                  body: IndexedStack(
+                    index: _index,
+                    children: <Widget>[
+                      Contacts(client: client),
+                      Album(client: client),
+                      SelectFile(client: client),
+                    ],
+                  ));
+            } else {
+              // if (snapshot.connectionState == ConnectionState.waiting) {
+              return new CircularProgressIndicator();
+              // }else
+            }
+          }),
     );
   }
 }
